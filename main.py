0585418554a4dd8a5c6c2a786c68cc30244c4876
Adrian Ban <pikapovwva@gmail.com>
2026-05-12 09:29:22 +0200

    add basic functions without tests

--- main.py
+++ main.py
@@ -6,6 +6,20 @@ def concatenate_strings(*args):
     
     return concatenated
 
+def get_string_length(string):
+    return len(string)
+
+def reverse_string(string):
+    return string[::-1]
+
+def count_vowels(string):
+    vowels = 'aeiouAEIOU'
+    count = 0
+    for char in string:
+        if char in vowels:
+            count += 1
+    return count
+
 # example_input = 'asd', 'qwe', 'example', 'test'
 print(concatenate_strings('asd', 'qwe', 'example', 'test'))
 
